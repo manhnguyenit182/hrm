2d70144db59ac68e9ece5104a23f16ec9bf5d94e
manhnguyenit182 <manhnguyenit182@gmail.com>
2025-09-09 18:09:36 +0700

    Revert "fix eslint config to ignore prisma client files"
    This reverts commit 6ac5a69c5de007f5b13a2ac7ee255d254140a0ca.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,11 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "src/db/prisma/**/*",
-    ],
-  },
 ];
 
 export default eslintConfig;
